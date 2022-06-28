9723770e54d80a79564ae0c9c03c8ef3e912d3eb
caiooliveirah09 <leelros@hotmail.com>
2022-06-28 15:46:09 -0300

    i solved requirement 5

--- src/App.js
+++ src/App.js
@@ -15,18 +15,47 @@ class App extends React.Component {
       cardRare: '',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
 
-  onInputChange = ({ target }) => {
+  onInputChange = async ({ target }) => {
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [target.id]: value,
-    });
+    this.setState({ [target.id]: value },
+      () => this.setState({ isSaveButtonDisabled: this.validateForms() }));
   }
 
-  onSaveButtonClick = () => <p>a</p>;
+  // setStateAsync = (state) => new Promise((resolve) => { this.setState(state, resolve); })
+
+  validateForms = () => {
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage,
+      cardRare, // cardTrunfo, hasTrunfo,
+      /* isSaveButtonDisabled */ } = this.state;
+
+    const sum = Number(parseInt(cardAttr1, 10)
+      + parseInt(cardAttr2, 10) + parseInt(cardAttr3, 10));
+
+    const sumMax = 210;
+    const AttrMin = 0;
+    const AttrMax = 90;
+
+    if (cardName.length > 0
+      && cardImage.length > 0
+      && cardDescription.length > 0
+      && Number(cardAttr1) <= AttrMax && Number(cardAttr1) >= AttrMin
+      && Number(cardAttr2) <= AttrMax && Number(cardAttr2) >= AttrMin
+      && Number(cardAttr3) <= AttrMax && Number(cardAttr3) >= AttrMin
+      && sum <= sumMax
+      && cardRare.length > 0) {
+      return false;
+    }
+    return true;
+  }
+
+  onSaveButtonClick = (event) => {
+    event.preventDefault();
+  }
 
   render() {
     const { cardName, cardDescription, cardAttr1,
